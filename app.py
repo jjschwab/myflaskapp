530d39a6257b963f919f1c19320acf1921973d4b
jjschwab <buckschwab@gmail.com>
2024-05-06 21:23:14 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, request, jsonify, render_template, send_from_directory
+import video_processing_refactored as vp
+import json
+import os
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/process_video', methods=['POST'])
 def process_video():
     data = request.get_json()
@@ -21,10 +32,10 @@ def process_video():
 
     # Mock descriptions based on category choice for demo
     description_phrases = {
-        "1": ["Skier jumping off a snow ramp", "Person skiing down a snowy mountain", "Close-up of skis on snow", "Skiing through a snowy forest", "Skier performing a spin",
+        "1": [""Skier jumping off a snow ramp", "Person skiing down a snowy mountain", "Close-up of skis on snow", "Skiing through a snowy forest", "Skier performing a spin",
             "Point-of-view shot from a ski helmet", "Group of skiers on a mountain", "Skier sliding on a rail", "Snow spraying from skis", "Skier in mid-air during a jump",
             "Person being interviewed after an event", "People in a crowd cheering", "Sitting inside of a vehicle", "Skaters standing around a ramp", "People standing around at an event",
-            "Commercial break", "People having a conversation", "Person in a helmet talking to the camera", "person facing the camera", "People introducing the context for a video"],  # Example phrases for categories
+            "Commercial break", "People having a conversation", "Person in a helmet talking to the camera", "person facing the camera", "People introducing the context for a video""],  # Example phrases for categories
         "2": ["Dramatic scene description"],
         "3": ["Calm scene description"]
     }
@@ -39,3 +50,11 @@ def process_video():
         return jsonify({'error': 'Failed to process final video'}), 500
 
     return jsonify({'message': 'Video processed successfully', 'video_filename': os.path.basename(final_video_info['path'])})
+
+@app.route('/downloads/<path:filename>', methods=['GET'])
+def download(filename):
+    return send_from_directory(app.static_folder, filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
