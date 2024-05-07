8a7a20e2de6c6e8d896e0e2c2ef9e60fdb2a111a
jjschwab <buckschwab@gmail.com>
2024-05-06 19:35:07 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template
 import video_processing_refactored as vp
-import json
 import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -29,9 +28,19 @@ def process_video():
     if not scene_frames:
         return jsonify({'error': 'Failed to extract frames'}), 500
 
-    first_scene_info = next(iter(scene_frames.values()))
-    saved_clip = vp.save_clip(video_path, first_scene_info, os.path.join(app.static_folder, 'videos'), 0)
+    # Classify and categorize scenes
+    description_phrases = ["Skier jumping off a snow ramp", "Person skiing down a snowy mountain", "Close-up of skis on snow", "Skiing through a snowy forest", "Skier performing a spin",
+            "Point-of-view shot from a ski helmet", "Group of skiers on a mountain", "Skier sliding on a rail", "Snow spraying from skis", "Skier in mid-air during a jump",
+            "Person being interviewed after an event", "People in a crowd cheering", "Sitting inside of a vehicle", "Skaters standing around a ramp", "People standing around at an event",
+            "Commercial break", "People having a conversation", "Person in a helmet talking to the camera", "person facing the camera", "People introducing the context for a video"]  # Placeholder phrases
+    scene_categories = vp.classify_and_categorize_scenes(scene_frames, description_phrases)
+    if not scene_categories:
+        return jsonify({'error': 'Failed to classify scenes'}), 500
 
+    # Save just the first scene to simplify
+    first_scene_info = next(iter(scene_categories.values()))
+    saved_clip = vp.save_clip(video_path, first_scene_info, os.path.join(app.static_folder, 'videos'), 1)
+    
     if not saved_clip:
         return jsonify({'error': 'No valid clips were selected or could be saved.'}), 500
     
