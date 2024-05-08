1b64f552986525f97dbd9580b1b7dd7bd963eca7
jjschwab <buckschwab@gmail.com>
2024-05-08 09:46:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, render_template, send_from_directory
 import video_processing_refactored as vp
 import json
 import os
-import base64
 import cv2
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -54,18 +53,40 @@ def process_video():
     top_action_scenes = sorted(action_scenes, key=lambda x: x['confidence'], reverse=True)[:20]
     best_clips = sorted(top_action_scenes, key=lambda x: x['duration'], reverse=True)[:10]
 
-    scene_details = [{
+     scene_details = [{
         'scene_number': list(categorized_scenes.keys())[list(categorized_scenes.values()).index(scene)] + 1,
         'duration': scene['duration'],
         'start_time': scene['start_time'],
         'end_time': scene['end_time'],
         'description': scene['best_description'],
         'first_frame': base64.b64encode(cv2.imencode('.jpg', scene['first_frame'])[1]).decode()
-    } for scene in best_clips]
+    } 
+    for scene in best_clips:
+        return jsonify(scene_details)
 
-    return jsonify(scene_details)
 
+@app.route('/concatenate_clips', methods=['POST'])
+def concatenate_clips():
+    data = request.get_json()
+    selected_indices = data.get('selected_indices')
+    video_path = data.get('video_path')
+
+    clips_info = [scene for i, scene in enumerate(best_clips) if i in selected_indices]
+    clip_paths = [vp.save_clip(video_path, scene, os.path.join(app.static_folder, 'videos'), i)['path'] for i, scene in enumerate(clips_info)]
+
+    final_video_info = vp.process_video(clip_paths, os.path.join(app.static_folder, 'videos', 'final_video.mp4'))
+    if 'path' not in final_video_info:
+        return jsonify({'error': 'Failed to process final video'}), 500
+
+    return jsonify({'message': 'Video processed successfully', 'video_filename': os.path.basename(final_video_info['path'])})
+
+@app.route('/downloads/<path:filename>', methods=['GET'])
+def download(filename):
+    return send_from_directory(app.static_folder, filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+
 
